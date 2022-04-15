b39c54abecd5be954937988a66b9e10765a2c7e5
chengzhihui007 <469870113@qq.com>
2022-04-15 18:13:30 +0800

    Update utils.js

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,14 +1,6 @@
 let OSS = require('ali-oss');
 
-// let client = new OSS({
-//   accessKeyId: 'LTAI4Fj3s66HrAkfegPKNEZw',
-//   accessKeySecret: 'YpvC3P76I3iiTUM5Vc5YumMXlFoGLU',
-// });
 let client = new OSS({
-  region: 'oss-cn-beijing',
-  accessKeyId: 'LTAI4Fj3s66HrAkfegPKNEZw',
-  accessKeySecret: 'YpvC3P76I3iiTUM5Vc5YumMXlFoGLU',
-  bucket: 'hxg-file-cdn'
 });
 
 exports.ossput = async function (storeAs, file) {
